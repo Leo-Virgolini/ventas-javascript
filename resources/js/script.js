e451bf92d125b3bddc0347a3edecc9868a157d77
Leo <leo.virgolini@gmail.com>
2022-09-30 22:47:15 -0300

    actualizado con objetos y arrays

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,36 +1,119 @@
-const productos = [1000, 300, 500, 600, 900];
-
-const calcularTotal = () => {
-    let total = 0;
-    let continuar;
-    let producto;
-    let cantidad;
-    do {
-        producto = undefined;
-        cantidad = undefined;
-        continuar = undefined;
-        while (isNaN(producto) || producto < 1 || producto > 5) {
-            producto = Number(prompt("Ingresa N° del producto:"));
-            if (isNaN(producto) || producto < 1 || producto > 5)
-                alert("Ingresa un número entre 1 y 5.");
-        }
-        while (isNaN(cantidad) || cantidad < 1) {
-            cantidad = Number(prompt("Ingresa cantidad del producto:"));
-            if (isNaN(cantidad) || cantidad < 1)
-                alert("Ingresa un número mayor a 0.");
-        }
-        total += productos[producto - 1] * cantidad;
-
-        while (continuar != "s" && continuar != "n") {
-            continuar = prompt("¿Desea seguir agregando productos (s/n)?");
-            console.log(continuar);
-            if (continuar != "s" && continuar != "n")
-                alert("Ingresa 's' o 'n'.");
-        }
-        // console.log(total);
-    } while (continuar == "s");
-
-    return total;
+class Producto {
+    constructor(id, nombre, precio, stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
 }
 
-alert("Total a pagar: $" + calcularTotal());
+class Item {
+    constructor(producto, cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+}
+
+class Carrito {
+    constructor() {
+        this.items = [];
+    }
+
+    agregarProductos() {
+        let continuar;
+        do {
+            continuar = undefined;
+            let producto;
+            let cantidad;
+            let item;
+            while (!producto) {
+                producto = this.buscarProducto(Number(prompt("Ingresa N° del producto:")));
+            }
+            while (isNaN(cantidad) || cantidad < 1) {
+                cantidad = Number(prompt("Ingresa cantidad del producto:"));
+                if (isNaN(cantidad) || cantidad < 1)
+                    alert("Ingresa un número mayor a 0.");
+            }
+
+            if (item = this.items.find(item => item.producto.id == producto.id))
+                item.cantidad += cantidad;
+            else
+                this.items.push(new Item(producto, cantidad));
+
+            while (continuar != "s" && continuar != "n") {
+                continuar = prompt("¿Desea seguir agregando productos (s/n)?");
+                if (continuar != "s" && continuar != "n")
+                    alert("Ingresa 's' o 'n'.");
+            }
+        } while (continuar == "s");
+        console.log(this.items);
+    }
+
+    buscarProducto(numeroProducto) {
+        if (isNaN(numeroProducto)) {
+            alert("Ingresa un número válido.");
+            return undefined;
+        }
+        let producto = productos.find(producto => producto.id == numeroProducto);
+        if (!producto) {
+            alert("El producto no existe.");
+            return undefined;
+        }
+        else
+            return producto;
+    }
+
+    calcularTotal() {
+        return this.items.reduce((acumulador, item) => acumulador + (item.producto.precio * item.cantidad), 0);
+    }
+
+    calcularTotalIva() {
+        return this.calcularTotal() * 1.21;
+    }
+}
+
+const productos = [new Producto(1, "Olla", 1000, 100), new Producto(2, "Vaso", 300, 100), new Producto(3, "Cubiertos", 500, 100), new Producto(4, "Jarra", 600, 100), new Producto(5, "Sartén", 900, 100)];
+
+let carrito = new Carrito();
+carrito.agregarProductos();
+alert("Total a pagar: $" + carrito.calcularTotal() + "\n" +
+    "Total con IVA: $" + carrito.calcularTotalIva());
+
+
+// const agregarProductos = () => {
+//     let total = 0;
+//     let continuar;
+//     let numeroProducto;
+//     let cantidad;
+//     let carrito = [];
+//     do {
+//         numeroProducto = undefined;
+//         cantidad = undefined;
+//         continuar = undefined;
+//         while (!(producto = existe(numeroProducto))) {
+//             numeroProducto = Number(prompt("Ingresa N° del producto:"));
+//             if (!existe(numeroProducto))
+//                 alert("Ingresa un número válido.");
+//         }
+//         while (isNaN(cantidad) || cantidad < 1) {
+//             cantidad = Number(prompt("Ingresa cantidad del producto:"));
+//             if (isNaN(cantidad) || cantidad < 1)
+//                 alert("Ingresa un número mayor a 0.");
+//         }
+//         carrito.push = productos[numeroProducto - 1];
+//         total += productos[numeroProducto - 1] * cantidad;
+
+//         while (continuar != "s" && continuar != "n") {
+//             continuar = prompt("¿Desea seguir agregando productos (s/n)?");
+//             console.log(continuar);
+//             if (continuar != "s" && continuar != "n")
+//                 alert("Ingresa 's' o 'n'.");
+//         }
+//         // console.log(total);
+//     } while (continuar == "s");
+
+//     return total;
+// }
+
+
+
